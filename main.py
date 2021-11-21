5ccc9934bac25e63bb83730ddc2181cfd7742e52
BiancaDeHaan <biancad88@yahoo.com>
2021-11-21 17:22:15 -0500

    11202021Montana-8

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def main_menu():
 
     student_account_txt.create_mycollege_users()
     student_account_txt.create_student_accounts()
-
+    db_commands.print_database(connection)
     user_story = start_options.succ_story("Student_story.txt")
     # log_in_status = False
     # Welcome everyone to InCollege, a story of success student
